b0e0d02eceee5b671cab0ec913b43a15e973e2f6
LiHanChen <lihan.lh.chen@ptc-nec.com.tw>
2026-01-29 13:47:12 +0800

    Add AddSongsListUi some space

--- app/src/main/java/com/lihan/vibeplayer/music_list/presentation/addsong/AddSongsScreen.kt
+++ app/src/main/java/com/lihan/vibeplayer/music_list/presentation/addsong/AddSongsScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,9 +21,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -83,6 +90,9 @@ fun AddSongsScreen(
     modifier: Modifier = Modifier,
     playlistId: Int?=null
 ) {
+    val density = LocalDensity.current
+    var okButtonHeight by remember { mutableStateOf(0.dp) }
+
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -168,12 +178,18 @@ fun AddSongsScreen(
                         )
 
                     }
+                    item {
+                        Spacer(Modifier.height(okButtonHeight))
+                    }
                 }
 
             }
             if(state.selectedAudios.isNotEmpty() || playlistId != null){
                 VPButton(
                     modifier = Modifier
+                        .onSizeChanged{
+                            okButtonHeight = with(density) { it.height.toDp()}
+                        }
                         .widthIn(max = 480.dp)
                         .fillMaxWidth()
                         .align(Alignment.BottomCenter)
@@ -211,7 +227,10 @@ private fun AddSongsScreenPreview() {
                         duration = it.toLong() * 10000,
                         isSelected = it % 3 == 0
                     )
-                }
+                },
+                selectedAudios = listOf(
+                    "1","2"
+                )
             ),
             onAction = {}
         )
